d32f4843a4f8896f60f4a9a854c20bc4ddb61201
ghibl <48108700+ghibl@users.noreply.github.com>
2019-04-17 13:33:10 -0700

    Update Mat345ProjectPart3.m

--- Mat345ProjectPart3.m
+++ Mat345ProjectPart3.m
@@ -1,14 +1,15 @@
 %% Part 3 
 A=imread("kharing.jpg");
 % rotate the image in the x-y plane
-R=[cosd(30),sind(30),0,0;-sind(30),cosd(30),0,0;0,0,1,0;0,0,0,1];
-R2=[1,2,0,0;3,1,0,0;0,0,1,0;0,0,0,1];
-R3=R*R2;
+R=[cosd(100),sind(100),0,0;-sind(100),cosd(100),0,0;0,0,1,0;0,0,0,1];
+R1=[5,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1];
+R2=[1,-2,0,0;0,1,0,0;0,0,1,0;0,0,0,1];
+R3=(R2*R1)*R;
 rform=affine3d(R3);
 B=imwarp(A,rform);
 imshow(B)
 
-% rotating back
+% Rotating back
 D=inv(R3);
 dform=affine3d(D);
 C=imwarp(B,dform);
